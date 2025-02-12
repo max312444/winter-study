37b25b0bc0021f9a4c09d7bf280190f85a3d2009
wonjun <max12max@g.yju.ac.kr>
2025-02-12 17:44:32 +0900

    0212 commit

--- study5.py
+++ study5.py
@@ -1,13 +1,17 @@
+num = 1
+
 try:
     print("1")
     
-    raise KeyboardInterrupt
+    if num == 1:
+        raise KeyboardInterrupt
 
     print("2")
-
-    print("3")
-    
-except Exception:
-    print("3.1")
+except KeyboardInterrupt:
+    print("4")
+else:
+    print("5")
+finally:
+    print("6")
     
 print("7")
