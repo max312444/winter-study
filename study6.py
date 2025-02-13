bd62bc3de0788850eb1f8d9f4fb8cbc1d5c031ff
wonjun <max12max@g.yju.ac.kr>
2025-02-14 00:31:56 +0900

    0214 commit

---
+++ study6.py
@@ -0,0 +1,17 @@
+num = 2
+
+try:
+    print("1")
+    
+    if num == 1:
+        raise KeyboardInterrupt
+
+    print("2")
+except KeyboardInterrupt:
+    print("4")
+else:
+    print("5")
+finally:
+    print("6")
+    
+print("7")
